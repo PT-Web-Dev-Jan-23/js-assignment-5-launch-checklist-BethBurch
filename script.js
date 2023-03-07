73c00f165476a0b6bc3f98c8e81b023acb67db62
Beth Burch <beth.burch@gmail.com>
2023-03-06 18:22:58 -0600

    final with sudo coding

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+window.addEventListener("load", function () {
+// WINDOW LOAD EVENT
+    let list = document.getElementById("faultyItems");
+// CREATES A FAULTY ITEMS ID VARIABLE FOR SCRIPT HELPER
+    let form = document.querySelector("form");
+// THIS IS A VARIABLE FOR SELECTING ANY ID NAMED "FORM" IN THE WHOLE DOCUMENT 
+    
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        let pilotInput = document.querySelector("input[name=pilotName]").value;
+        let copilotInput = document.querySelector("input[name=copilotName]").value;
+        let fuelInput = document.querySelector("input[name=fuelLevel]").value;
+        let cargoInput = document.querySelector("input[name=cargoMass]").value;
+// varible name is wherever in the whole document you select  the input from the user associated with the [name]
+// and then send that input to the server.
+// THE LAST PART OF THE FUCNTION IS TO CALL THE FORMSUBMISSION FUNCTION FROM SCRIPTHELPER.JS WHEERE EVERYTHING RUNS.
+// !!!!! THIS IS WHERE THE "SUBMIT CLICK" HAPPENS 
+        formSubmission(document, list, pilotInput, copilotInput, fuelInput, cargoInput);
+    });
+    
+    let listedPlanets;
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
+        let selectedPlanet = pickPlanet(listedPlanets);
+        addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image);
+    })
+// SHOWS WHICH PICTURE WILL BE SHOWING AT A GIVEN TIME BY CALLING THE FETCH AND THE PICK PLANET RANDOMIZER
+
 });
